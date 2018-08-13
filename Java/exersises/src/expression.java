a7bb88b90cd14ba9472b36a6de0b4df4077fe3aa
Primaxm <primaxm@gmail.com>
2018-08-13 12:15:45 +0300

    more java files

--- Java/exersises/src/expression.java
+++
@@ -1,7 +0,0 @@
-public class Expression {
-        public static void main(String[] args) {
-            double val = (30 + 21) * 0.5 * (35 - 12 - 0.5);
-            double sqrVal = Math.pow(val, 2);
-            System.out.println(sqrVal);
-        }
-}
